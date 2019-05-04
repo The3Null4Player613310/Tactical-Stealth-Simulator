d467b75776a53fa2e6496fe26e008130a2934fbe
Nathan Munn <the.3null.4player.6.13.3.10.@gmail.com>
2019-05-03 21:07:32 -0300

    Updated Movement
    revamped movement

--- gamemode/init.lua
+++ gamemode/init.lua
@@ -151,9 +151,10 @@ function PlayerStaminaHandler() --WIP
 		end
 		
 		--set move modifier --note using half of stamina is most effective
-		Player[PlayerID]:SetRunSpeed( 200 + (120 * (Player[PlayerID]:GetNWFloat( "stamina", 0 ) / 100) * (Player[PlayerID]:Health() / 100))) --320
-		Player[PlayerID]:SetMaxSpeed( (80 +(240 * Player[PlayerID]:GetNWFloat( "stamina", 0 ) / 100))*(Player[PlayerID]:Health()/100)) --320
-		Player[PlayerID]:SetWalkSpeed( (80 + (120 * Player[PlayerID]:GetNWFloat( "stamina", 0 ) / 100))*(Player[PlayerID]:Health()/100)) --200
+		Player[PlayerID]:SetMaxSpeed((80+(240*(Player[PlayerID]:GetNWFloat( "stamina", 0 ) / 100)))*(Player[PlayerID]:Health() / 100))
+		Player[PlayerID]:SetRunSpeed((80+(180*(Player[PlayerID]:GetNWFloat( "stamina", 0 ) / 100)))*(Player[PlayerID]:Health() / 100))
+		Player[PlayerID]:SetWalkSpeed((80+(120*(Player[PlayerID]:GetNWFloat( "stamina", 0 ) / 100)))*(Player[PlayerID]:Health() / 100))
+		Player[PlayerID]:SetCrouchedWalkSpeed(((-1+0.60000002384186)*(Player[PlayerID]:Health() / 100))+1)
 		
 	end
 end
